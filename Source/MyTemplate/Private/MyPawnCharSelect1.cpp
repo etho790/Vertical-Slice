161f10bcc7e7f2ac11408d03964fd10f48a91d52
Christian <mdscjdf@gmail.com>
2019-10-25 09:39:27 +1300

    no message

---
+++ Source/MyTemplate/Private/MyPawnCharSelect1.cpp
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyPawnCharSelect1.h"
+
+// Sets default values
+AMyPawnCharSelect1::AMyPawnCharSelect1()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AMyPawnCharSelect1::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMyPawnCharSelect1::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AMyPawnCharSelect1::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
